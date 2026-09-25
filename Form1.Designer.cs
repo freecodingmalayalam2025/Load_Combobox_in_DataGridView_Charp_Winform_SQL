25a188d910b0fbb4fcfee1bbe730c5a248048a73
Free Coding <freecodingmalayalam2025@gmail.com>
2026-09-25 17:13:41 +0530

    initial commit
    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp_LoadGridView_with_Combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.CustId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CustId,
+            this.Name});
+            this.DGV.Location = new System.Drawing.Point(38, 95);
+            this.DGV.Name = "DGV";
+            this.DGV.RowHeadersWidth = 51;
+            this.DGV.RowTemplate.Height = 24;
+            this.DGV.Size = new System.Drawing.Size(684, 270);
+            this.DGV.TabIndex = 0;
+            // 
+            // CustId
+            // 
+            this.CustId.DataPropertyName = "CustId";
+            this.CustId.HeaderText = "Cust Id";
+            this.CustId.MinimumWidth = 6;
+            this.CustId.Name = "CustId";
+            this.CustId.Width = 125;
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Name.DataPropertyName = "Name";
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DGV);
+            this.MaximizeBox = false;
+           // this.Name = "Form1";
+            this.Text = "Load ComboBox in DataGridView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp_LoadGridView_with_Combobox
+{
+    public partial class Form1 : Form
+    {
+        string strcon = @"Data Source=LAPTOP-R9MRDLN3\SQLEXPRESS;Initial Catalog=SampleDB;Integrated Security=True;Encrypt=False";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void LoadCustomer()
+        {
+            using(SqlConnection con = new SqlConnection(strcon))
+            {
+                con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("select * from Country", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                DataGridViewComboBoxColumn dgvcbc = new DataGridViewComboBoxColumn();
+                dgvcbc.HeaderText = "Country";
+                dgvcbc.Name = "CountryName";
+                dgvcbc.DataSource = dt;
+                dgvcbc.DisplayMember = "Name";
+                dgvcbc.ValueMember = "C_id";
+                dgvcbc.DataPropertyName = "CountryId";
+
+                DGV.Columns.Add(dgvcbc);
+
+
+                SqlDataAdapter sda1 = new SqlDataAdapter("select * from customer", con);
+                DataTable dt1 = new DataTable();
+                sda1.Fill(dt1);
+                DGV.DataSource = dt1;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCustomer();
+        }
+    }
+}
